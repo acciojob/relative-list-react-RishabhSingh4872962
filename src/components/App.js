4f1ac4592ff4ee1a44590a86aaaf757b98b74de3
Rishabh <rs4872962@gmail.com>
2024-04-29 10:11:29 +0530

    bug solve

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ const App = () => {
   const [list, setList] = useState(["sumit","vinnet","boby"])
   return (
     <div id="main">
-       <ol key={"relativeList"}>
+       <ol key={"relativeList"} id='relativeList'>
              {list.map((name,indx)=>{
-              return <li key={`relativeListItem${indx}`}>{name}</li>
+              return <li key={`relativeListItem${indx}`} id={`relativeListItem${indx}`}>{name}</li>
              })}
               </ol>
     </div>
